61a177dcdcfe67c8b9425beb5aa625d9f8c8faed
ttakuma17 <ttanimiya@gmail.com>
2024-12-21 23:46:20 +0900

    formatの一時変数を関数化する

--- index.js
+++ index.js
@@ -37,13 +37,16 @@ function volumeCreditsFor(aPerformance) {
   return result;
 }
 
+function format(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency", currency: "USD", minimumFractionDigits: 2
+  }).format(aNumber);
+}
+
 function statement(invoice) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency", currency: "USD", minimumFractionDigits: 2
-  }).format;
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
