d185ecd51b6ed65ba571b5eb6f70962221257c17
ttakuma17 <ttanimiya@gmail.com>
2024-12-21 23:48:26 +0900

    formatがやってることはusdへの変更だった

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ function volumeCreditsFor(aPerformance) {
   return result;
 }
 
-function format(aNumber) {
+function usd(aNumber) {
   return new Intl.NumberFormat("en-US", {
     style: "currency", currency: "USD", minimumFractionDigits: 2
-  }).format(aNumber);
+  }).format(aNumber / 100);
 }
 
 function statement(invoice) {
@@ -51,12 +51,12 @@ function statement(invoice) {
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
     // 注文の内訳を出力
-    result += ` ${playFor(perf).name}: ${format(
-      amountFor(perf, playFor(perf)) / 100)} (${perf.audience} seats)\n`;
+    result += ` ${playFor(perf).name}: ${usd(
+      amountFor(perf, playFor(perf)))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf, playFor(perf));
   }
 
-  result += `Amount owed is ${format(totalAmount / 100)}\n`
+  result += `Amount owed is ${usd(totalAmount)}\n`
   result += `You earned ${volumeCredits} credits\n`
   return result;
 }
