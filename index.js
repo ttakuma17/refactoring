2166efc5fb43d1ce7635fbb9009aa81400429e5d
ttakuma17 <ttanimiya@gmail.com>
2024-12-21 23:42:27 +0900

    変数名をわかりやすく

--- index.js
+++ index.js
@@ -26,15 +26,15 @@ function playFor(aPerformance) {
 }
 
 
-function volumeCreditsFor(perf) {
-  let volumeCredits = 0;
+function volumeCreditsFor(aPerformance) {
+  let result = 0;
   // ボリューム特典のポイントを加算
-  volumeCredits += Math.max(perf.audience - 30, 0);
+  result += Math.max(aPerformance.audience - 30, 0);
   // 喜劇のときに10人につき、さらにポイントを加算
-  if ("comedy" === playFor(perf).type) {
-    volumeCredits += Math.floor(perf.audience / 5);
+  if ("comedy" === playFor(aPerformance).type) {
+    result += Math.floor(aPerformance.audience / 5);
   }
-  return volumeCredits;
+  return result;
 }
 
 function statement(invoice) {
